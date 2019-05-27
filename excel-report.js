b4bfb42c9a0157db2abc1e3def9dc8c3d026d0a1
hubert977 <hubert.wozniak977@gmail.com>
2019-05-27 14:55:30 +0200

    2705

--- excel-report.js
+++ excel-report.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
 class excel 
 {
     constructor()
@@ -14,10 +16,10 @@ class excel
     }
     initialize()
     {
+        console.log('asdfdasf');
         let correctArray = [];
         const connection = mysql.createConnection(this.configure);
-        connection.connect();
-            connection.query('SELECT id_comparator from weighting ORDER BY id_comparator DESC LIMIT 10', (error, results, fields) => {
+            connection.query('SELECT id_comparator from weighting ORDER BY id_comparator DESC LIMIT 220', (error, results, fields) => {
             if (error) throw error;
                 let results_json = JSON.stringify(results);
                 let resultjson = JSON.parse(results_json);
@@ -31,17 +33,17 @@ class excel
                          arrayData.push(resultjson[i]);
                          correctArray = [...new Set(arrayvalue)];
                     }
-                    connection.end();
                     that.cyclesiterable(correctArray);
+                    console.log(correctArray);
                   })
         });
     }
     cyclesiterable(correctArray)
     {
         const connection = mysql.createConnection(this.configure);
-        connection.connect();
         for(let i=0; i<correctArray.length; i++)
         {
+
             connection.query(`SELECT comparatorreport.st_deviation , comparatorreport.average_diff,weighting.ID,weighting.guid,weighting.ARCHIVAL,weighting.date,weighting.mass_in_g,weighting.tare_in_g,weighting.mass_in_unit,weighting.unit,weighting.UNIT_CAL,weighting.precision_act,weighting.precision_Cal,weighting.mass_is_stab,weighting.Mass_Air_Density_Correction,weighting.id_comparator,weighting.Cycle,weighting.AirDensity,weighting.THBTemperature,weighting.THBPressure,weighting.THBHumidity from weighting,comparatorreport where id_comparator=${correctArray[i]} AND weighting.id_comparator=comparatorreport.ID`, (error, results, fields)=>{
                     if (error) throw error;
                     let results_json = JSON.stringify(results);
@@ -50,15 +52,29 @@ class excel
                     let worksheet = workbook.getWorksheet(1);
                     let environmental_conditions_start_column = 3;
                     let row_23 = 23;
-                    for(let j=0; i<resultjson.length; i++)
-                    {
-                        row_23.getCell(environmental_conditions_start_column).value = resultjson[j].THBTemperature;
-                        row_23.commit();
-                        row_23 = row_23 + 7;
-                    }
+                    let CountRow = 15;
+                   
                     
+                    let Conditiontemp = 55;           
+                    for(let j=0; j<resultjson.length; j++)
+                    {
+                        let row_15 = worksheet.getRow(CountRow);
+                        row_15.getCell(environmental_conditions_start_column).value = resultjson[j].THBTemperature;
+                            console.log(CountRow);
+                         
+                        row_15.commit();
+                        row_15 = row_23 + 7;
+                        if(CountRow == Conditiontemp)
+                        {
+                            Conditiontemp = Conditiontemp + 55;
+                            CountRow = CountRow + 15;
+                        }
+                        else 
+                        {
+                            CountRow = CountRow + 8;
+                        }
+                    }
                     workbook.xlsx.writeFile(`files/data.xlsx`);
-                    connection.end();
                 }).catch((err)=>{
                         console.log(err);
                 })
